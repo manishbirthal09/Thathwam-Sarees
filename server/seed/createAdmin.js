a5a0767c39d665953b2546ae6ffd254c72d3f4b9
manish birthal <manishbirthal4717@gmail.com>
2026-08-25 18:37:16 +0530

    added email and password to .env

--- server/seed/createAdmin.js
+++ server/seed/createAdmin.js
@@ -1,34 +1,4 @@
 
-// import mongoose from "mongoose";
-// import bcrypt from "bcryptjs";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import Admin from "../models/Admin.js";
-
-// const run = async () => {
-//   await mongoose.connect(process.env.MONGO_URI);
-
-//   const email = "admin@thathwamsaree.com";
-//   const password = "ThathwamAdminPassword2026"; 
-
-//   const hashed = await bcrypt.hash(password, 10);
-
-//   const admin = await Admin.findOneAndUpdate(
-//     { email },
-//     { password: hashed },
-//     { new: true }
-//   );
-
-//   if (!admin) {
-//     console.log("Admin not found");
-//   } else {
-//     console.log("Password updated successfully");
-//   }
-
-//   await mongoose.disconnect();
-// };
-
-// run();
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
@@ -38,8 +8,8 @@ import Admin from "../models/Admin.js";
 const run = async () => {
   await mongoose.connect(process.env.MONGO_URI);
 
-  const email = "admin@thathwamsaree.com";
-  const password = "ThathwamAdminPassword2026";
+  const email = process.env.ADMIN_EMAIL;
+  const password = process.env.ADMIN_PASSWORD;
 
   const hashed = await bcrypt.hash(password, 10);
 

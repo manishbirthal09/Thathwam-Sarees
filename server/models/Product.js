0fbdb633b1342044c6a6a28fe1b3669356d49413
manish birthal <manishbirthal4717@gmail.com>
2026-08-29 11:02:11 +0530

    Fix product schema: remove unused originalPrice, add discountPrice

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    originalPrice: { type: Number, required: true },
+    discountPrice: { type: Number, required: true },
     category: {
   type: mongoose.Schema.Types.ObjectId,
   ref: "Category",
